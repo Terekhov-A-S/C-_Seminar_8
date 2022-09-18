9c0ae806a9372776e54c3de98d3d824f9d0f0da7
KoT <Kotto@student.su>
2022-09-18 10:29:34 +0400

    added task_62

---
+++ Task_62/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Console.Clear();
+int[,] array = new int[4, 4];
+int count = new Random().Next(1, 100);
+for (int horizontNum = 0; horizontNum < array.GetLength(0); horizontNum++)
+{
+    for (int verticalNum = 0; verticalNum < array.GetLength(0); verticalNum++)
+    {
+        if (horizontNum == 0) { array[horizontNum, verticalNum] = count; count++; }
+        if (horizontNum == 1 & verticalNum < 3) { array[horizontNum, verticalNum] = count + 7; count++; }
+        if (horizontNum == 1 & verticalNum == 3) { array[horizontNum, verticalNum] = count - 3; }
+        if (horizontNum == 2 & verticalNum == 0) { array[horizontNum, verticalNum] = count + 3; }
+        if (horizontNum == 2 & verticalNum > 0 & verticalNum < 3) { array[horizontNum, verticalNum] = count + 8; count--; }
+        if (horizontNum == 2 & verticalNum == 3) { array[horizontNum, verticalNum] = count; }
+        if (horizontNum == 3 & verticalNum <= 4) { array[horizontNum, verticalNum] = count + 4; count--; }
+        Console.Write($"{array[horizontNum, verticalNum]} ");
+    }
+    Console.WriteLine();
+}
